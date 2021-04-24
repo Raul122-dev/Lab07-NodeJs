bead11686ffff78c7f62f615157ba76b502a9dff
Raul122 <victor.huaycho@tecsup.edu.pe>
2021-04-24 18:27:53 -0500

    third commit

--- index.js
+++ index.js
@@ -30,7 +30,17 @@ app.get('/api/persons', (request, response) => {
 app.get('/info', (request, response) => {
     response.send('<p>Phonebook has info for '+ persons.length +' people</p>' +
                 '<p>'+ new Date( Date.parse('2012-01-26T13:51:50.417-07:00') ) +'</p>')
-  })
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+})  
 
 const PORT = 3001
 app.listen(PORT, () => {
