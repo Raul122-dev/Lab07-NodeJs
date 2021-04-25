a5795842e3232be623d23917099b78cdcbfcb3ec
Raul122 <victor.huaycho@tecsup.edu.pe>
2021-04-24 19:22:24 -0500

    fifth commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
         id:1,
@@ -22,6 +24,11 @@ let persons = [
         number:"39-23-6423122"
     }
 ]
+
+const generarID = () =>{
+    return Math.floor(Math.random() * (100000 - 1)) + 1;
+}
+
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
@@ -47,6 +54,25 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
   
     response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+  
+    if (!body) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+  
+    const person = {
+        id: generarID(),
+        name: body.name,
+        number: body.number
+    }
+  
+    persons = persons.concat(person)
+    response.json(person)
   })
 
 
